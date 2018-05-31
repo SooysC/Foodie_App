7c36be4dcbe23df90b6d69316577b0da8773d407
Mike Shi <mikehxshi@gmail.com>
2018-05-31 19:59:44 -0400

    Finished initial listview with added pictures and gradient over some better placeholder images. Deleted miwok template pictures

--- app/src/main/java/com/example/team/foodie/MainActivity.java
+++ app/src/main/java/com/example/team/foodie/MainActivity.java
@@ -18,16 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
         // Create a list of mainPageRecipes
         ArrayList<MainPageRecipe> mainPageRecipes = new ArrayList<MainPageRecipe>();
-        mainPageRecipes.add(new MainPageRecipe("Recipe One", "1", R.drawable.androidparty));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Two", "2", R.drawable.number_two));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Three", "3", R.drawable.number_three));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Four", "4", R.drawable.number_four));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Five", "5", R.drawable.number_five));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Six", "6", R.drawable.number_six));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Seven", "7", R.drawable.number_seven));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Eight", "8", R.drawable.number_eight));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Nine", "9", R.drawable.number_nine));
-        mainPageRecipes.add(new MainPageRecipe("Recipe Ten", "10", R.drawable.number_ten));
+        mainPageRecipes.add(new MainPageRecipe("Recipe One", "1", R.drawable.smoothie));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Two", "2", R.drawable.avocadotoast));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Three", "3", R.drawable.eggsbenedict));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Four", "4", R.drawable.smokedsalmon));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Five", "5", R.drawable.smoothie));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Six", "6", R.drawable.avocadotoast));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Seven", "7", R.drawable.eggsbenedict));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Eight", "8", R.drawable.smokedsalmon));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Nine", "9", R.drawable.smoothie));
+        mainPageRecipes.add(new MainPageRecipe("Recipe Ten", "10", R.drawable.avocadotoast));
 
         MainPageRecipeAdapter adapter =
                 new MainPageRecipeAdapter(this, mainPageRecipes);
